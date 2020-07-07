8ba029e62a86f7a76332b43f915c5be211fe6c58
keiko kondo <keiko.ayaka@icloud.com>
2020-07-07 10:30:07 +0900

    不要な文言を削除した

--- app/controllers/says_controller.rb
+++ app/controllers/says_controller.rb
@@ -1,28 +1,20 @@
 class SaysController < ApplicationController
   before_action :set_say, only: [:show, :edit, :update, :destroy]
 
-  # GET /says
-  # GET /says.json
   def index
     @says = Say.all
   end
 
-  # GET /says/1
-  # GET /says/1.json
   def show
   end
 
-  # GET /says/new
   def new
     @say = Say.new
   end
 
-  # GET /says/1/edit
   def edit
   end
 
-  # POST /says
-  # POST /says.json
   def create
     @say = Say.new(say_params)
       if params[:back]
@@ -34,8 +26,6 @@ class SaysController < ApplicationController
       end
   end
 
-  # PATCH/PUT /says/1
-  # PATCH/PUT /says/1.json
   def update
     if @say.update(say_params)
       redirect_to says_path,notice:"つぶやきを編集しました！"
@@ -44,8 +34,6 @@ class SaysController < ApplicationController
     end
   end
 
-  # DELETE /says/1
-  # DELETE /says/1.json
   def destroy
     @say.destroy
       redirect_to says_url, notice: 'つぶやきを削除しました！'
@@ -57,12 +45,10 @@ class SaysController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_say
     @say = Say.find(params[:id])
   end
 
-    # Only allow a list of trusted parameters through.
   def say_params
     params.require(:say).permit(:content)
   end
